e2b79c4694b8515e04ecd1a85d10f419c4a669e6
Zelalem <zzewdie@gmail.com>
2023-10-07 02:11:27 +0300

    0x06_regex_3b

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 
 arg = ARGV[0]
-if arg =~ /hbt{1,4}n/
-  match = arg.scan(/hbt{1,4}n/)
+if arg =~ /hbt{1,}n/
+  match = arg.scan(/hbt{1,}n/)
   matches = match
   puts matches
 end
