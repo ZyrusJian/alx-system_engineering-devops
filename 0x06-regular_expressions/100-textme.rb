144a02bbcfc1610c688e5fef9736ed1e3e602adc
Zelalem <zzewdie@gmail.com>
2023-10-07 04:29:54 +0300

    0x06_regex_100b

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,9 +1,14 @@
 #!/usr/bin/env ruby
 
-arg = ARGV[0]
-if arg =~ /from:(\S+)/ && arg =~ /to:(\S+)/ && arg =~ /flags:([\d:-]+)/
-  sender = $1
-  receiver = $2
-  flags = $3
+require 'oniguruma'
+
+regex = /\[from:(?<sender>[^\]]+)\] \[to:(?<receiver>[^\]]+)\] \[flags:(?<flags>[^\]]+)\]/
+input = ARGV[0]
+match = regex.match(input)
+
+if match
+  sender = match[:sender]
+  receiver = match[:receiver]
+  flags = match[:flags]
   puts "#{sender},#{receiver},#{flags}"
 end
